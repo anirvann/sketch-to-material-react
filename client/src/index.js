2dc728c8ee5c5b1c911c7ecb18b1c7f32fb7c2f7
ANIRVANN DAS <anirvann@gmail.com>
2019-09-07 13:51:45 +0530

    index file

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Organism from './components/organisms/Organism';
+import Organism from './components/organisms/rganism';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<Organism />, document.getElementById('root'));
